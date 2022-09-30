e6771ad1eacad75c0155ff6579df6c088868bfd2
Andrew G <vardantexcode@gmail.com>
2022-09-30 18:15:50 -0400

    Current Stable

--- GAMES/Hoppy_Hop/Level_Select.gd
+++ GAMES/Hoppy_Hop/Level_Select.gd
@@ -19,6 +19,10 @@ func _on_Level_Three_pressed():
 	get_tree().change_scene("res://Levels/Level3.tscn")
 
 
+func _on_Level_Four_pressed():
+	get_tree().change_scene("res://Levels/Level4.tscn")
+
+
 
 func _on_Return_Button_pressed():
 	get_tree().change_scene("res://Main_Menu.tscn")
