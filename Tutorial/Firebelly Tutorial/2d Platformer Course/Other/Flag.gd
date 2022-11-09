26b2a6afd98a1a2c6cfa0efdd194d71bea44a4c7
Andrew G <vardantexcode@gmail.com>
2022-11-09 17:45:55 -0500

    Scene Transitions

--- Tutorial/Firebelly Tutorial/2d Platformer Course/Other/Flag.gd
+++ Tutorial/Firebelly Tutorial/2d Platformer Course/Other/Flag.gd
@@ -4,7 +4,14 @@ signal player_won
 
 func _ready() -> void:
 	$Area2D.connect("area_entered", self, "on_area_entered")
+	$TransitionLayer/ColorRect.visible = false
+
+func _process(delta: float) -> void:
+	pass
+	
 	
 
 func on_area_entered(_area2d):
+	$TransitionLayer/ColorRect.visible = true
+	$AnimationPlayer.play("TRANSITION_PLAYER")
 	emit_signal("player_won")
--- Tutorial/Firebelly Tutorial/2d Platformer Course/Scenes/BaseLevel.gd
+++ Tutorial/Firebelly Tutorial/2d Platformer Course/Scenes/BaseLevel.gd
@@ -1,17 +1,32 @@
 extends Node
 
+signal coin_total_changed
 
 var playerScene = preload("res://Scenes/Player.gd")
 var spawnPosition = Vector2.ZERO
 var currentPlayerNode = null
-
+var totalCoins = 0 
+var collectedCoins = 0
 
 
 func _ready() -> void:
 	#Set spawn where player is in the Scene
 	spawnPosition = $Player.global_position
 	register_player($Player)
+	
+	#Get root of overall scene tree of coin.tscn group
+	coin_total_changed(get_tree().get_nodes_in_group("coin").size())
+	
 
+func coin_total_changed(newTotal):
+	totalCoins = newTotal
+	totalCoins = get_tree().get_nodes_in_group("coin").size()
+
+#Create a function to collect coins
+func coin_collected():
+	collectedCoins += 1
+	print(totalCoins, collectedCoins)
+	emit_signal("coin_total_changed", totalCoins, collectedCoins)
 
 #Listen to the player death 
 func register_player(player):
